1d87d9565bb4eb30ba3eca6de9154ad1ad1b830f
Nima Dini <nima.dini@utexas.edu>
2016-09-01 11:40:49 -0500

    fixed indentation

--- example.sh
+++ example.sh
@@ -1,10 +1,10 @@
-RANDOOP="https://github.com/randoop/randoop.git"	# Repository URL.
-RSHA="28b8b10163b2d9dcd786b5276a3c4ff2f977302e"		# Commit ID.
-RANDOOP_DIR="randoop"								# Randoop source directory.
-RANDOOP_JAR="randoop-all-3.0.3.jar"					# Randoop-all jar file.
-TIME="30"											# Generation time.
-OUTPUT_DIR="rgts"									# Output directory -> Randomly Generated Tests (rgts).
-TESTS_PER_FILE="50"									# Maximum number of tests to write to each JUnit file.
+RANDOOP="https://github.com/randoop/randoop.git"  # Repository URL.
+RSHA="28b8b10163b2d9dcd786b5276a3c4ff2f977302e"   # Commit ID.
+RANDOOP_DIR="randoop"                             # Randoop source directory.
+RANDOOP_JAR="randoop-all-3.0.3.jar"               # Randoop-all jar file.
+TIME="30"                                         # Generation time.
+OUTPUT_DIR="rgts"                                 # Output directory -> Randomly Generated Tests (rgts).
+TESTS_PER_FILE="50"                               # Maximum number of tests to write to each JUnit file.
 
 # Do not clone randoop if it already exists.
 if [ ! -d ${RANDOOP_DIR} ]; then
